c49deac9215bd943763352b9b36367f52a3849bf
Davut Turug <44504087+Davuttrg@users.noreply.github.com>
2022-02-21 17:14:59 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'task-manager-a7b5d',
-    appId: '1:1094680868324:web:0a11df5ef0be92013783f1',
-    storageBucket: 'task-manager-a7b5d.appspot.com',
-    apiKey: 'AIzaSyBnEe5rP7nsUImDarUTGlnWpoPjGskfdis',
-    authDomain: 'task-manager-a7b5d.firebaseapp.com',
-    messagingSenderId: '1094680868324',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
   production: true
 };
